e05d621d04d9d1ef5f22284782b499833cb959cd
Flávio Arêas <vflavio9@gmail.com>
2024-12-05 17:12:43 -0300

    chore: initial app structure

---
+++ GHFollowers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GHFollowers
+//
+//  Created by Flávio Arêas on 05/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemPink
+    }
+
+
+}
+
