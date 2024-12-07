11a97466b82f79d195ad188ee9c660fe004baa25
Flávio Arêas <vflavio9@gmail.com>
2024-12-07 16:37:00 -0300

    feat: add EmptyStateView to followers list

--- GHFollowers/Screens/FollowersListVC.swift
+++ GHFollowers/Screens/FollowersListVC.swift
@@ -55,8 +55,17 @@ class FollowersListVC: UIViewController {
             
             switch result {
             case .success(let followers):
-                if followers.count < 100 { self.hasMoreFollowers = false }
+                if followers.count < 100 { self.hasMoreFollowers = false }                
                 self.followers.append(contentsOf: followers)
+                
+                if self.followers.isEmpty {
+                    let message = "This user doesn't have any followers. Go follow them 😀"
+                    DispatchQueue.main.async {
+                        self.showEmptyStateView(with: message, in: self.view)
+                    }
+                    return
+                }
+                
                 self.updateData()
                 break
                 
