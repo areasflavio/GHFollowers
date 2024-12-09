8ffce750737d8607c05fc377b2edbeba43c6055a
Flávio Arêas <vflavio9@gmail.com>
2024-12-09 09:34:16 -0300

    feat: create UserInfoHeader view

---
+++ GHFollowers/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  GHFollowers
+//
+//  Created by Flávio Arêas on 09/12/24.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
