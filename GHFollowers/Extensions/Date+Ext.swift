72354ce20b320b4e542fd2e61a53ac64c3cc7a75
Flávio Arêas <vflavio9@gmail.com>
2024-12-10 17:35:53 -0300

    feat: format and display date

---
+++ GHFollowers/Extensions/Date+Ext.swift
@@ -0,0 +1,17 @@
+//
+//  Date+Ext.swift
+//  GHFollowers
+//
+//  Created by Flávio Arêas on 10/12/24.
+//
+
+import Foundation
+
+extension Date {
+    func convertToMonthYearFormat() -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MMM yyyy"
+        
+        return dateFormatter.string(from: self)
+    }
+}
