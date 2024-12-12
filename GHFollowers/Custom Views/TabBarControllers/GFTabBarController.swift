d275744074c33d9ffb40a02b75c779190925a476
Flávio Arêas <vflavio9@gmail.com>
2024-12-12 09:11:09 -0300

    refactor: move TabBar config to its own file

---
+++ GHFollowers/Custom Views/TabBarControllers/GFTabBarController.swift
@@ -0,0 +1,32 @@
+//
+//  GFTabBarController.swift
+//  GHFollowers
+//
+//  Created by Flávio Arêas on 12/12/24.
+//
+
+import UIKit
+
+class GFTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        UITabBar.appearance().tintColor = .systemGreen
+        viewControllers = [createSearchNC(), createFavoritesNC()]
+    }
+    
+    func createSearchNC() -> UINavigationController {
+        let searchVC = SearchVC()
+        searchVC.title = "Search"
+        searchVC.tabBarItem = UITabBarItem(tabBarSystemItem: .search, tag: 0)
+        return UINavigationController(rootViewController: searchVC)
+    }
+    
+    func createFavoritesNC() -> UINavigationController {
+        let favoritesVC = FavoritesVC()
+        favoritesVC.title = "Favorites"
+        favoritesVC.tabBarItem = UITabBarItem(tabBarSystemItem: .favorites, tag: 1)
+        return UINavigationController(rootViewController: favoritesVC)
+    }
+
+}
