b56a3f0dc92df125a89494af94f9d14571d96a79
Flávio Arêas <vflavio9@gmail.com>
2024-12-12 16:53:15 -0300

    refactor: move data loading view extension to its own file

--- GHFollowers/Screens/FollowersListVC.swift
+++ GHFollowers/Screens/FollowersListVC.swift
@@ -11,7 +11,7 @@ protocol FollowersListVCDelegate: AnyObject {
     func didRequestFollowers(for username: String)
 }
 
-class FollowersListVC: UIViewController {
+class FollowersListVC: GFDataLoadingVC {
     
     enum Section {
         case main
